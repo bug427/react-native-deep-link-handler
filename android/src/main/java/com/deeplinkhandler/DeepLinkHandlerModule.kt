a93d27b4682c3a98aadba9573a53a4e085cdaeb7
Žiga Hvalec <zigahvalec@gmail.com>
2024-12-19 22:42:42 +0100

    feat: remove native code

--- android/src/main/java/com/deeplinkhandler/DeepLinkHandlerModule.kt
+++
@@ -1,7 +0,0 @@
-package com.deeplinkhandler
-
-import com.facebook.react.bridge.ReactApplicationContext
-import com.facebook.react.module.annotations.ReactModule
-
-@ReactModule(name = DeepLinkHandlerModule.NAME)
-class DeepLinkHandlerModule(reactContext: ReactApplicationContext) :
--- ios/DeepLinkHandler.h
+++
@@ -1,6 +0,0 @@
-
-#import "generated/RNDeepLinkHandlerSpec/RNDeepLinkHandlerSpec.h"
-
-@interface DeepLinkHandler : NSObject <NativeDeepLinkHandlerSpec>
-
-@end
